608e63015b09f4c657db41d4e6b191f560583663
muralikrishnasai36@gmail.com <muralikrishnasai36@gmail.com>
2022-11-18 16:39:51 -0800

    updated 2221

--- november/2221_find_triangular_sum_of_arr.py
+++ november/2221_find_triangular_sum_of_arr.py
@@ -11,3 +11,10 @@ class Solution:
             new_nums.append(ele)
         
         return self.triangularSum( new_nums )
+    
+class Solution:
+    def triangularSum(self, nums: List[int]) -> int:
+
+        while len(nums)>1:
+            nums = [(a+b)%10 for(a,b) in zip(nums,nums[1:])]
+        return nums[0]
