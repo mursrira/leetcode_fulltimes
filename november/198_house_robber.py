f7153c83347495058c5e565f7e08cf47bc731470
muralikrishnasai36@gmail.com <muralikrishnasai36@gmail.com>
2022-11-23 15:04:12 -0800

    added 198

---
+++ november/198_house_robber.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        rob1, rob2 = 0, 0
+
+        #[ rob1, rob2, n, n+1, n+2, n+3 ..... ]
+        # At every level we have 2 options
+        # option1: n+rob1
+        # option2: rob2
+
+        for n in nums:
+            temp = max( n+rob1, rob2 )
+            rob1 = rob2
+            rob2 = temp
+        
+        return rob2
