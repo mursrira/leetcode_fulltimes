a00fe64cc412bbcbfccc365e1f634cb63d71bdd1
muralikrishnasai36@gmail.com <muralikrishnasai36@gmail.com>
2022-11-23 02:28:40 -0800

    updated 1268

--- november/1268_search_sugg_sys.py
+++ november/1268_search_sugg_sys.py
@@ -1,18 +1,18 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-
-
+        
         res = []
-        products.sort()
 
+        products.sort()
         l, r = 0, len(products)-1
 
         for i in range(len(searchWord)):
+
             c = searchWord[i]
 
-            while l<=r and ( len(products[l]) <= i or products[l][i] != c ):   
-                l += 1    
-            while l<=r and ( len(products[r]) <= i or products[r][i] != c ):   
+            while l <= r and ( (len(products[l])<=i ) or (products[l][i] != c) ):
+                l += 1
+            while l <= r and ( (len(products[r])<=i ) or (products[r][i] != c) ):
                 r -= 1
 
             res.append([])
@@ -21,4 +21,4 @@ class Solution:
             for j in range(min(3,remain)):
                 res[-1].append(products[l+j])
 
-        return res   
+        return res
