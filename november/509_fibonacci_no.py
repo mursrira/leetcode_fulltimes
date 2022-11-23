ec234157648e8959234ce7627aaf8f4d786807be
muralikrishnasai36@gmail.com <muralikrishnasai36@gmail.com>
2022-11-22 21:03:53 -0800

    updated 509

--- november/509_fibonacci_no.py
+++ november/509_fibonacci_no.py
@@ -13,3 +13,10 @@ class Solution:
             prev1 = res
 
         return res
+    
+class SolutionRecurvsive:
+    def fib_recursive(self, n: int) -> int:
+        if n == 0: return 0
+        if n == 1: return 1
+
+        return self.fib_recursive(n-1)+self.fib_recursive(n-2)
