d27434515a592ea66006480a0fb41cdbcdb8d282
muralikrishnasai36@gmail.com <muralikrishnasai36@gmail.com>
2022-11-26 01:01:07 -0800

    updated readme added 1004

---
+++ november/1004_max_consecutive_ones_3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+
+        start, end = 0, 0
+
+        while( end < len(nums) ):
+
+            if nums[end] == 0:
+                k -= 1
+            
+            if(k<0):
+
+                if nums[start]==0:
+                    k += 1
+                
+                start += 1
+            
+            end += 1
+        
+        return end - start
