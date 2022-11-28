adc3bf904b7c38dd6a1f5f3fe17fc90e582fc6ef
muralikrishnasai36@gmail.com <muralikrishnasai36@gmail.com>
2022-11-28 13:22:01 -0800

    updated 62

--- november/62_uniq_paths.py
+++ november/62_uniq_paths.py
@@ -14,3 +14,12 @@ class Solution:
             row = newRow
         
         return row[0]
+    
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1]*m]*n
+
+        for row in range(1, len(dp)):
+            for col in range(1, len(dp[row])):
+                dp[row][col] = dp[row-1][col]+dp[row][col-1]
+        return dp[-1][-1]
