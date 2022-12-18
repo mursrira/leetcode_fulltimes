174d259b8740d635b3ada29d38b0acb194dea658
muralikrishnasai36@gmail.com <muralikrishnasai36@gmail.com>
2022-12-18 00:25:50 -0800

    updated 125

--- november/125_valid_palindrome.py
+++ november/125_valid_palindrome.py
@@ -16,3 +16,25 @@ class Solution:
             else:
                 return False
         return True
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = len(s)-1
+        st, ed = 0, len(s)-1
+
+        while(st<=ed):
+
+            while(st<=l and not(s[st].isalnum())):
+                st += 1
+            while(0<=ed and not(s[ed].isalnum())):
+                ed -= 1
+            
+            if(st>l and ed<0):
+                break
+            
+            if(s[st].lower()==s[ed].lower()):
+                st += 1
+                ed -= 1
+            else:
+                return False
+        return True
