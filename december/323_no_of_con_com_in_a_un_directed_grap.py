7cdaf3ba35c85061b7c8b67218da24d56c6edfa3
muralikrishnasai36@gmail.com <muralikrishnasai36@gmail.com>
2022-12-31 14:45:07 -0800

    added union find for 323

--- december/323_no_of_con_com_in_a_un_directed_grap.py
+++ december/323_no_of_con_com_in_a_un_directed_grap.py
@@ -31,3 +31,30 @@ class Solution:
                 res=bfs(i,res)
 
         return cnt
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+
+        parent=list(range(n))
+
+        def find(x):
+            if x!=parent[x]:
+                parent[x]=find(parent[x])
+            return parent[x]
+
+        def union(x,y):
+            p_x=find(x)
+            p_y=find(y)
+
+            if(p_x!=p_y):
+                parent[p_y]=p_x
+        
+        for x,y in edges:
+            union(x,y)
+
+        store=collections.defaultdict(list)
+
+        for idx,p in enumerate(parent):
+            store[find(p)].append(idx)
+        
+        return len(store.keys())
--- november/62_uniq_paths.py
+++ november/62_uniq_paths.py
@@ -23,3 +23,11 @@ class Solution:
             for col in range(1, len(dp[row])):
                 dp[row][col] = dp[row-1][col]+dp[row][col-1]
         return dp[-1][-1]
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp=[[1]*m]*n
+        for r in range(1,len(dp)):
+            for c in range(1,len(dp[r])):
+                dp[r][c]=dp[r][c-1]+dp[r-1][c]
+        return dp[n-1][m-1]
